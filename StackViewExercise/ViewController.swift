38fad98eb1457a80b8a9217b13016b946ed4b226
Ryuichi kawamonzen <ryuichikawamonzen@Ryuichi-no-MacBook-Pro.local>
2020-07-07 22:19:48 +0900

    first commit

---
+++ StackViewExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewExercise
+//
+//  Created by Ryuichi kawamonzen on 2020/07/07.
+//  Copyright © 2020 Ryuichi kawamonzen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
